b04b7f43cb802e78bfdee0cf6dc18c481a9b3f10
MaycolYM11 <maycolcardonasanchez14@gmail.com>
2024-06-25 12:59:59 -0500

    Se agrega a las variables de entorno a a config db

--- src/models/sigfviDBModelo.js
+++ src/models/sigfviDBModelo.js
@@ -28,6 +28,7 @@ const DB_DATABASE = process.env.DB_DATABASE;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
+const DB_PORT = process.env.DB_PORT;
 
 // Conexion base de datos
 const db = mysql.createConnection({
@@ -35,6 +36,7 @@ const db = mysql.createConnection({
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
+  port:DB_PORT,
   ssl: {
     ca: caCert,
   },
